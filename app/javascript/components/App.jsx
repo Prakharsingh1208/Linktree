a048afdfc0a7f3e4408d6fb1960908e3f1d52bb8
prakhar1208 <prakhar.singh@dailyrounds.org>
2025-12-11 05:54:29 +0530

    Integrate vite + React.js for frontend

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function App() {
+    let name = "Prakhar"
+    return (
+        <div>
+            <h1>Hello from React to  {name}</h1>
+            <p>Vite + Rails + React is working!</p>
+        </div>
+    )
+}
